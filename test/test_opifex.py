d1fd7eb767116399866c7e59fd236b05a4f88153
262871 <61919302+262871@users.noreply.github.com>
2023-12-16 23:55:32 +0100

    Created opifex package

--- test/test_opifex.py
+++ test/test_opifex.py
@@ -1,2 +1,10 @@
+import sys, pathlib
+sys.path.append(pathlib.Path.cwd().parent.resolve())
+
+import src.opifex as opifex
+
 def test_pytest():
     assert True
+
+def test_opifex():
+    assert opifex
